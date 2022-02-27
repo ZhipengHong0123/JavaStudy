5271a2dda08d1f8ca7ed70dcae7fdae617e9751b
Zhipeng Hong <h19990123@126.com>
2022-02-27 12:39:51 +0800

    Data Structure

---
+++ leetcode/DataStructure/src/String/GroupAnagrams.java
@@ -0,0 +1,22 @@
+package String;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length==0){
+            return new ArrayList<>();
+        }
+        Map<String,List> ans = new HashMap<>();
+        for (String s: strs){
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = String.valueOf(c);
+            if (!ans.containsKey(key)){
+                ans.put(key,new ArrayList<>());
+                ans.get(key).add(s);
+            }
+        }
+        return new ArrayList<>(ans.values());
+    }
+}
